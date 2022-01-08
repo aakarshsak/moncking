248c829b5e3fc71dc5bf4d4a16b46be11db90ac2
Aakarsh Sinha <aakarshsak@gmail.com>
2022-01-08 19:49:12 +0530

    For testing the base exchangeInfo url

--- routes/data.js
+++ routes/data.js
@@ -8,16 +8,16 @@ router.get("/", async (req, res) => {
 
     let result;// = "REs";
     const test = 'https://reqres.in/api/users?page=2';
-    const binance = "https://api.binance.com/api/v3/time";
+    const binance = "https://api.binance.com/api/v3/exchangeInfo";
     try {
         const response = await axios.get(binance);
         // console.log(response);
         result = response;
     } catch (error) {
-        console.error(error);
-        result = error;
+        console.log("Status", );
+        res.status(error.response.status).send(error);
     }
-    console.log(result);
+    // console.log(result);
 
     res.send(result.data);
 });
